ab5338c5397d7f3130d9ce32b74b1a41646f77eb
Yazan Alsolyman <132354795+4yznx@users.noreply.github.com>
2024-12-16 23:58:11 +0300

    Update AdminController.cs

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,40 +1,37 @@
-﻿using BarberShop.Data;
+using BarberShop.Data;
 using BarberShop.Models;
-using BarberShop.ViewModels;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace BarberShop.Controllers
 {
-
-    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly BarberDbContext _context;
-        private readonly UserManager<Kullanici> _userManager;
 
-        public AdminController(BarberDbContext context, UserManager<Kullanici> userManager)
+        public AdminController(BarberDbContext context)
         {
             _context = context;
-            _userManager = userManager;
         }
 
+        // --- INDEX ---
         public IActionResult Index()
         {
             ViewData["Layout"] = "_LayoutAdmin.cshtml";
             return View();
         }
 
+        // --- CALISAN LISTESI ---
         public IActionResult CalisanListesi()
         {
             var calisanlar = _context.Calisanlar
-                .Include(c => c.Islemler)
+                .Include(c => c.CalisanIslemler)
+                .ThenInclude(ci => ci.Islem)
                 .ToList();
             return View(calisanlar);
         }
 
+        // --- CALISAN EKLE ---
         public IActionResult CalisanEkle()
         {
             ViewBag.Islemler = _context.Islemler.ToList();
@@ -42,24 +39,27 @@ namespace BarberShop.Controllers
         }
 
         [HttpPost]
-        public IActionResult CalisanEkle(Calisan calisan, List<int> SelectedIslemIds)
+        public IActionResult CalisanEkle(Calisan calisan, List<int> SecilenIslemIds)
         {
             if (ModelState.IsValid)
             {
-                calisan.Islemler = new List<Islem>();
-
-                foreach (var id in SelectedIslemIds)
-                {
-                    var islem = _context.Islemler.Find(id);
-                    if (islem != null)
-                    {
-                        calisan.Islemler.Add(islem);
-                    }
-                }
-
+                // Add Calisan
                 _context.Calisanlar.Add(calisan);
                 _context.SaveChanges();
-                TempData["msj"] = $"{calisan.Adi} {calisan.Soyadi} adlı çalışan başarıyla eklendi.";
+
+                // Add selected Islem entries in CalisanIslem
+                foreach (var islemId in SecilenIslemIds)
+                {
+                    var calisanIslem = new CalisanIslem
+                    {
+                        CalisanId = calisan.CalisanID,
+                        IslemID = islemId
+                    };
+                    _context.Add(calisanIslem);
+                }
+
+                _context.SaveChanges();
+                TempData["msj"] = $"{calisan.Adi} {calisan.Soyadi} başarıyla eklendi.";
                 return RedirectToAction("CalisanListesi");
             }
 
@@ -67,19 +67,19 @@ namespace BarberShop.Controllers
             return View(calisan);
         }
 
-
+        // --- CALISAN DUZENLE ---
         public IActionResult CalisanDuzenle(int id)
         {
             var calisan = _context.Calisanlar
-                .Include(c => c.Islemler)
-                .FirstOrDefault(c => c.CalisanId == id);
+                .Include(c => c.CalisanIslemler)
+                .FirstOrDefault(c => c.CalisanID == id);
 
             if (calisan == null)
-            {
                 return NotFound();
-            }
 
             ViewBag.Islemler = _context.Islemler.ToList();
+            ViewBag.SelectedIslemler = calisan.CalisanIslemler.Select(ci => ci.IslemID).ToList();
+
             return View(calisan);
         }
 
@@ -89,25 +89,25 @@ namespace BarberShop.Controllers
             if (ModelState.IsValid)
             {
                 var existingCalisan = _context.Calisanlar
-                    .Include(c => c.Islemler)
-                    .FirstOrDefault(c => c.CalisanId == calisan.CalisanId);
+                    .Include(c => c.CalisanIslemler)
+                    .FirstOrDefault(c => c.CalisanID == calisan.CalisanID);
 
                 if (existingCalisan == null)
-                {
                     return NotFound();
-                }
 
+                // Update basic details
                 existingCalisan.Adi = calisan.Adi;
                 existingCalisan.Soyadi = calisan.Soyadi;
 
-                existingCalisan.Islemler.Clear();
-                foreach (var id in SelectedIslemIds)
+                // Update CalisanIslem entries
+                existingCalisan.CalisanIslemler.Clear();
+                foreach (var islemId in SelectedIslemIds)
                 {
-                    var islem = _context.Islemler.Find(id);
-                    if (islem != null)
+                    existingCalisan.CalisanIslemler.Add(new CalisanIslem
                     {
-                        existingCalisan.Islemler.Add(islem);
-                    }
+                        CalisanId = calisan.CalisanID,
+                        IslemID = islemId
+                    });
                 }
 
                 _context.SaveChanges();
@@ -119,16 +119,16 @@ namespace BarberShop.Controllers
             return View(calisan);
         }
 
+        // --- CALISAN SIL ---
         public IActionResult CalisanSil(int id)
         {
             var calisan = _context.Calisanlar
-                .Include(c => c.Islemler)
-                .FirstOrDefault(c => c.CalisanId == id);
+                .Include(c => c.CalisanIslemler)
+                .ThenInclude(ci => ci.Islem)
+                .FirstOrDefault(c => c.CalisanID == id);
 
             if (calisan == null)
-            {
                 return NotFound();
-            }
 
             return View(calisan);
         }
@@ -137,8 +137,8 @@ namespace BarberShop.Controllers
         public IActionResult CalisanSilConfirmed(int id)
         {
             var calisan = _context.Calisanlar
-                .Include(c => c.Islemler)
-                .FirstOrDefault(c => c.CalisanId == id);
+                .Include(c => c.CalisanIslemler)
+                .FirstOrDefault(c => c.CalisanID == id);
 
             if (calisan != null)
             {
@@ -150,12 +150,14 @@ namespace BarberShop.Controllers
             return RedirectToAction("CalisanListesi");
         }
 
+        // --- ISLEM LISTESI ---
         public IActionResult IslemListesi()
         {
             var islemler = _context.Islemler.ToList();
             return View(islemler);
         }
 
+        // --- ISLEM EKLE ---
         public IActionResult IslemEkle()
         {
             return View();
@@ -169,11 +171,12 @@ namespace BarberShop.Controllers
                 _context.Islemler.Add(islem);
                 _context.SaveChanges();
                 TempData["msj"] = $"{islem.Adi} işlemi başarıyla eklendi.";
-                return RedirectToAction("IslemListesi");
+                return RedirectToAction("Index");
             }
             return View(islem);
         }
 
+        // --- ISLEM DUZENLE ---
         public IActionResult IslemDuzenle(int id)
         {
             var islem = _context.Islemler.Find(id);
@@ -192,11 +195,12 @@ namespace BarberShop.Controllers
                 _context.Islemler.Update(islem);
                 _context.SaveChanges();
                 TempData["msj"] = $"{islem.Adi} işlemi başarıyla güncellendi.";
-                return RedirectToAction("IslemListesi");
+                return RedirectToAction("Index");
             }
             return View(islem);
         }
 
+        // --- ISLEM SIL ---
         public IActionResult IslemSil(int id)
         {
             var islem = _context.Islemler.Find(id);
@@ -217,7 +221,7 @@ namespace BarberShop.Controllers
                 _context.SaveChanges();
                 TempData["msj"] = $"{islem.Adi} işlemi başarıyla silindi.";
             }
-            return RedirectToAction("IslemListesi");
+            return RedirectToAction("Index");
         }
     }
 }
