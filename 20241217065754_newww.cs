89256039775e7c401148184e699455e96fb4fac7
Yazan Alsolyman <132354795+4yznx@users.noreply.github.com>
2024-12-17 23:54:15 +0300

    Add files via upload

---
+++ 20241217065754_newww.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class newww : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Randevular",
+                columns: table => new
+                {
+                    RandevuID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RandevuTarihi = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Onaylandi = table.Column<bool>(type: "bit", nullable: false),
+                    CalisanID = table.Column<int>(type: "int", nullable: false),
+                    IslemID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Randevular", x => x.RandevuID);
+                    table.ForeignKey(
+                        name: "FK_Randevular_Calisanlar_CalisanID",
+                        column: x => x.CalisanID,
+                        principalTable: "Calisanlar",
+                        principalColumn: "CalisanID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Randevular_Islemler_IslemID",
+                        column: x => x.IslemID,
+                        principalTable: "Islemler",
+                        principalColumn: "IslemID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Randevular_CalisanID",
+                table: "Randevular",
+                column: "CalisanID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Randevular_IslemID",
+                table: "Randevular",
+                column: "IslemID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Randevular");
+        }
+    }
+}
